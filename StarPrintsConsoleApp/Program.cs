03a84d6bd93a0ca9d05ce37f4986ad589ad34d7e
Sachein <dev.sachein@yahoo.com>
2024-11-27 11:41:16 +0530

    Add project files.

---
+++ StarPrintsConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+public class starPrints()
+{
+    public void star(int n)
+    {
+        Console.WriteLine("STAR PRINT ");        
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        starPrints sp = new starPrints();
+        sp.star(5);
+    }
+}
+
